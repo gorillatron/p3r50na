09a262f34405288842bc461fde942771c6126a44
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2015-10-06 18:29:23 +0200

    werk

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/p3r50na.standalone.jar clojure.main -m p3r50na.core
--- src/p3r50na/core.clj
+++ src/p3r50na/core.clj
@@ -1,6 +1,6 @@
 (ns p3r50na.core
   (:require [com.stuartsierra.component :as component]
-            [p3r50na.system :refer [create-system]])
+            [p3r50na.system :refer [create-system]]))
 
 
 (defn -main [& args]
