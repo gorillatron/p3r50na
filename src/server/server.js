c88e3e449391e46f63da167ea71bad81a2cea433
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-04-23 16:17:44 +0200

    use monfig

--- src/server/server.js
+++ src/server/server.js
@@ -6,14 +6,14 @@ import reactRender     from './reactRender'
 
 
 export async function start(config) {
-  console.log(config)
+
   const server = new Koa()
 
   if(process.env.NODE_ENV === "production") {
     server.use(serve("assets/"))
   }
   if(process.env.NODE_ENV === "development") {
-    //webpackServer(server)
+    webpackServer(server)
   }
 
   server.use(reactRender)
